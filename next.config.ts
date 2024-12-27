91a2ffd40a603ddc119be200e852f44736ceea0c
Snehasish Mandal <mandalsneh97@gmail.com>
2024-12-28 01:53:13 +0530

    Updated teh next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    MAINTAINACE_MODE: process.env.MAINTAINACE_MODE,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
